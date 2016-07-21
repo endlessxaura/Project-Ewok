33b9a794569985db66accbb84edbaed66575736e
Benjamin Carlson <carlson.j.ben@gmail.com>
2016-07-21 11:39:13 -0400

    Completed authentication
    NOTE: This authentication system REQUIRES openssl. When putting this on
    the live server, be sure to include it

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -10,6 +10,7 @@ use Tymon\JWTAuth\Exceptions\JWTException;
 use Illuminate\Http\Request;
 use App\User;
 use Hash;
+use Validator;
 
 class AuthenticateController extends Controller
 {
@@ -31,9 +32,42 @@ class AuthenticateController extends Controller
     }
 
     public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|unique:user,email',
+            'password' => 'required|confirmed'
+            ]);
+
+        if($validator->fails()){
+            return response()->json(['error' => 'registration failed'], 400);
+        }
+
         User::create([
             'email' => $request->email,
             'password' => Hash::make($request->password)
             ]);
+    }
+
+    public function refreshToken(Request $request){
+        $token = JWTAuth::getToken();
+        if(!$token){
+            return $this->response->errorUnauthorized('Token is invalid');
+        }
+
+        try {
+            $refreshedToken = JWTAuth::refresh($token);
+        }   
+        catch(JWTException $ex){
+            return $this->response->error('Something went wrong');
+        }
+
+        return $refreshedToken;
+    }
+
+    public function destroyToken(Request $request){
+        $user = JWTAuth:::parseToken()->authenticate();
+        if(!$user){
+            $this->response->error('Token is invalid');
+        }  
+        $user->delete();
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,12 +19,44 @@ Route::get('/', function () {
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function($api){
+	//Authentication
 	$api->post('register', 'App\Http\Controllers\AuthenticateController@register');
 
 	$api->post('authenticate', 'App\Http\Controllers\AuthenticateController@authenticate');
 
+	$api->post('refreshToken', 'App\Http\Controllers\AuthenticateController@refreshToken');
+
+	//Geolocations
 	$api->group([], function($api){
 		$api->resource('geolocations', 'App\Http\Controllers\GeolocationController');
+	});
+
+	$api->group(['middleware' => 'api.auth'], function($api){
+		$api->get('users', function(){
+			return App\User::all();
+		});
+
+		$api->get('user', function(){
+			try{
+				$user = JWTAuth::parseToken()->toUser();
+
+				if(!$user){
+					return $this->response->errorNotFound('User not found');
+				}
+				else{
+					return $user;
+				}
+			}
+			catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $ex){
+				return $this->response->error('token is invalid');
+			}
+			catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $ex){
+				return $this->response->error('token expired');
+			}
+			catch (\Tymon\JWTAuth\Exceptions\TokenBlacklistedException $ex){
+				return $this->response->error('token blacklisted');
+			}
+		});
 	});
 });
 
