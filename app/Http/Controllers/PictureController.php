21accf029ef588e76aafbcca80ffd93e6461876d
Benjamin Carlson <carlson.j.ben@gmail.com>
2016-08-03 14:50:29 -0400

    Added storing, indexing, and showing pictures

--- app/Http/Controllers/PictureController.php
+++ app/Http/Controllers/PictureController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Picture;
 use App\Http\Requests;
+use App\Http\Controllers\Responses;
+use Illuminate\Http\Response;
+use Storage;
+use Validator;
+use File;
 
 class PictureController extends Controller
 {
@@ -13,9 +18,19 @@ class PictureController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        //PRE: request may contain 
+        //      id (The id of the model you are looking for)
+        //      model (the model you are looking for, eg. App\Farm)
+        if($request->has('id') && $request->has('model')){
+            return Picture::where('attached_id', '=', $request->input('id'))
+                ->where('attached_type', '=', $request->input('model'))
+                ->get();
+        }
+        else{
+            return Picture::all();
+        }
     }
 
     /**
@@ -36,7 +51,46 @@ class PictureController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //PRE: $request MUST contain the following
+        //      image (the file being uploaded)
+        //      attachedModel (model being attached to, eg. App\Farm)
+        //      attachedID = the ID of the attached model
+        //POST: stores the picture in the DB
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|file',
+            'attachedModel' => 'required',
+            'attachedID' => 'required|integer'
+            ]);
+        if(!$validator->fails() && $request->hasFile('image') && $request->file('image')->isValid()){
+            //Creating the model
+            $picture = Picture::create([
+                'attached_id' => $request->input('attachedID'),
+                'attached_type' => $request->input('attachedModel')
+                ]);
+
+            //Naming the file
+            $file = $request->file('image');
+            $attachedItem = $picture->attached;
+            if($attachedItem == null){
+                return Responses::BadRequest();
+            }
+            $modelName = substr($request->input('attachedModel'), 4);
+            $fileName = $modelName . "/" . $attachedItem->getKey() . '/' . time() . $file->getClientOriginalName();
+
+            //Storing the image
+            Storage::put(
+                $fileName,
+                File::get($file)
+                );
+
+            //Updating model and saving
+            $picture->filePath = $fileName;
+            $picture->save();
+            return Responses::Created($picture->pictureID);
+        }
+        else{
+            return Responses::BadRequest();
+        }
     }
 
     /**
@@ -47,7 +101,14 @@ class PictureController extends Controller
      */
     public function show($id)
     {
-        //
+        //POST: returns the specific picture asked for
+        $picture = Picture::find($id);
+        if($picture != null){
+            return $picture;
+        }
+        else{
+            return Responses::DoesNotExist();
+        }
     }
 
     /**
@@ -70,9 +131,56 @@ class PictureController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
-    }
+        //PRE: $request may contain the following
+        //      image (required)
+        //      attachedModel (model being attached to, eg. App\Farm)
+        //      attachedID = the ID of the attached model
+        //POST: stores the picture in the DB
 
+        // $picture = Picture::find($id);
+        // if($picture != null){
+        //     $validator = Validator::make($request->all(), [
+        //         'image' => 'required|file'
+        //         ]);
+        //     if(!$validator->fails() && $request->hasFile('image') && $request->file('image')->isValid()){
+        //         //Updating pic info
+        //         $picture->attached_id = $request->input('attachedID', $picture->attached_id);
+        //         $picture->attached_type = $request->input('attachedModel', $picture->attached_type);
+
+        //         //Naming the file
+        //         $file = $request->file('image');
+        //         $attachedItem = $picture->attached;
+        //         if($attachedItem == null){
+        //             return "Hello";
+        //             return Responses::BadRequest();
+        //         }
+        //         $modelName = substr($request->input('attachedModel'), 4);
+        //         $fileName = $modelName . "/" . $attachedItem->getKey() . '/' . time() . $file->getClientOriginalName();
+                
+
+        //         //Storing the image
+        //         Storage::put(
+        //             $fileName,
+        //             File::get($file)
+        //             );
+
+        //         //Remove original image
+        //         Storage::delete($picture->filePath);
+
+        //         //Updating model and saving
+        //         $picture->filePath = $fileName;
+        //         $picture->save();
+        //         return Responses::Created($picture->pictureID);
+        //     }
+        //     else{
+        //         return Responses::BadRequest();
+        //     }
+        // }
+        // else{
+        //     return Responses::DoesNotExist();
+        // }
+
+    }
     /**
      * Remove the specified resource from storage.
      *
@@ -81,6 +189,15 @@ class PictureController extends Controller
      */
     public function destroy($id)
     {
-        //
+        //POST: removes the picture in filesystem and DB
+        $picture = Picture::find($id);
+        if($picture != null){
+            Storage::delete($picture->filePath);
+            $picture->delete();
+            return Responses::Updated();
+        }
+        else{
+            return Responses::DoesNotExist();
+        }
     }
 }
