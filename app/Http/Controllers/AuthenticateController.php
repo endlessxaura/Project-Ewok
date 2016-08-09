a852e6fe516de103d07089c7a3e2260984b56767
Benjamin Carlson <carlson.j.ben@gmail.com>
2016-08-09 13:55:11 -0400

    Changed authenticate and user
    Getting rid of bugs

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -55,10 +55,10 @@ class AuthenticateController extends Controller
         }
 
         try {
-            $refreshedToken = JWTAuth::refresh($token);
+            $token = JWTAuth::refresh($token);
         }   
         catch(JWTException $ex){
-            return $this->response->error('Something went wrong');
+            return response()->json(["error" => 'Bad token']);
         }
 
         return response()->json(compact('token'));
@@ -77,24 +77,28 @@ class AuthenticateController extends Controller
         try {
 
             if (! $user = JWTAuth::parseToken()->authenticate()) {
-                return response()->json(['user_not_found'], 404);
+                return response()->json(['error' => 'user_not_found'], 404);
             }
 
         } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
 
-            return response()->json(['token_expired'], $e->getStatusCode());
+            return response()->json(['error' => 'token_expired'], $e->getStatusCode());
 
         } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
 
-            return response()->json(['token_invalid'], $e->getStatusCode());
+            return response()->json(['error' => 'token_invalid'], $e->getStatusCode());
 
         } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
 
-            return response()->json(['token_absent'], $e->getStatusCode());
+            return response()->json(['error' => 'token_absent'], $e->getStatusCode());
 
         }
 
+        $user->reviews;
+        $user->geolocations;
+        $user->pictures;
+
         // the token is valid and we have found the user via the sub claim
-        return response()->json(['userID' => $user->userID, "email" => $user->email]);
+        return $user;
     }
 }
